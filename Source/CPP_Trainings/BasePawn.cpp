a5e9337e40b2f3c3286009c1b809e403e67562bf
Ashkan Abedi <ashkan.gplay89@gmail.com>
2025-03-07 14:15:50 +0100

    Add main components and move functionality to BasePawn

--- Source/CPP_Trainings/BasePawn.cpp
+++ Source/CPP_Trainings/BasePawn.cpp
@@ -1,7 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Camera/CameraComponent.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "EnhancedInputComponent.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
@@ -9,13 +14,50 @@ ABasePawn::ABasePawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Component"));
+	RootComponent = CapsuleComponent;
+
+	BaseSkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Base Skeletal Mesh"));
+	BaseSkeletalMesh->SetupAttachment(RootComponent);
+
+	BaseCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Base Camera"));
+	BaseCamera->SetupAttachment(RootComponent);
+
+	AutoReceiveInput = EAutoReceiveInput::Player0;
+	
 }
 
 // Called when the game starts or when spawned
 void ABasePawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	PlayerStates = EPlayerStates::Normal;
+}
+
+
+void ABasePawn::MoveForward(const FInputActionValue& Value)
+{
+	if (PlayerStates == EPlayerStates::Normal)
+	{
+		AddMovementInput(this->GetActorForwardVector(), Value.GetMagnitude());
+		PlayerStates = EPlayerStates::Walking;
+	}
+}
+
+void ABasePawn::TurnRight(const FInputActionValue& Value)
+{
+	if (PlayerStates == EPlayerStates::Normal || PlayerStates == EPlayerStates::Walking)
+	{
+		AddControllerYawInput(Value.GetMagnitude());
+	}
+}
+
+void ABasePawn::TurnLeft(const FInputActionValue& Value)
+{
+	if (PlayerStates == EPlayerStates::Normal || PlayerStates == EPlayerStates::Walking)
+	{
+		AddControllerPitchInput(Value.GetMagnitude());
+	}
 }
 
 // Called every frame
@@ -29,6 +71,11 @@ void ABasePawn::Tick(float DeltaTime)
 void ABasePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+	EnhancedInputComponent->BindAction(InputMoveForward, ETriggerEvent::Triggered, this, &ABasePawn::MoveForward);
+	EnhancedInputComponent->BindAction(InputTurnRight, ETriggerEvent::Triggered, this, &ABasePawn::TurnRight);
+	EnhancedInputComponent->BindAction(InputTurnLeft, ETriggerEvent::Triggered, this, &ABasePawn::TurnLeft);
 
 }
 
--- Source/CPP_Trainings/BasePawn.h
+++ Source/CPP_Trainings/BasePawn.h
@@ -3,8 +3,24 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/Pawn.h"
 #include "BasePawn.generated.h"
+
+struct FInputActionValue;
+class UCapsuleComponent;
+class USkeletalMeshComponent;
+class UCameraComponent;
+class UInputAction;
+class UInputMappingContext;
+
+UENUM(BlueprintType)
+enum class EPlayerStates : uint8
+{
+	Normal,
+	Walking,
+	Turning
+};
 
 UCLASS()
 class CPP_TRAININGS_API ABasePawn : public APawn
@@ -14,6 +30,34 @@ class CPP_TRAININGS_API ABasePawn : public APawn
 public:
 	// Sets default values for this pawn's properties
 	ABasePawn();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UCapsuleComponent> CapsuleComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<USkeletalMeshComponent> BaseSkeletalMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<UCameraComponent> BaseCamera;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> InputMoveForward;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> InputTurnRight;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> InputTurnLeft;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pawn")
+	EPlayerStates PlayerStates;
+
+	void MoveForward (const FInputActionValue& Value);
+	void TurnRight (const FInputActionValue& Value);
+	void TurnLeft (const FInputActionValue& Value);
 
 protected:
 	// Called when the game starts or when spawned
